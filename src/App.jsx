89569d31b5dab237221302b15360c78e51a7d673
Neru <nenriquev1@gmail.com>
2023-06-12 08:28:25 +0200

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
 import "./App.css";
+import { useState } from "react";
+import FoodBox from "./components/FoodBox";
+import foodJson from "./foods.json"
+import { motion, AnimatePresence} from "framer-motion"
 
 function App() {
+
+  const [foods, setFoods] = useState(foodJson)
+  const [foodsDelete, setFoodDelete] = useState(foodJson)
+
+
+ 
+
+  const handleDelete = (id) => {
+    const result = foods.filter((food)=> food.id !== id)
+    const haSalido = foods.find((food) => food.id == id)
+    setFoods(result)
+    setFoodDelete(haSalido)
+  }
+
+  
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      {
+        foods.map((food, index) => (
+          <AnimatePresence key={index} initial={false}>
+            { 
+             (
+            <motion.div key={food.id} initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} >
+              <FoodBox data={food} key={index} onDelete={handleDelete}/>
+            </motion.div>)
+            }
+          </AnimatePresence>
+        ))
+      }
+     
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,25 @@
-// Your code here
+
+
+export default function FoodBox({ data, onDelete}) {
+
+ 
+
+  return (
+    <div >
+          <p>{data.name}</p>
+
+          <div style={{width: '300px', height: '200px', display:'flex'}}>
+            <img style={{objectFit: 'fill', width: '100%'}} src={data.image} alt={data.nombre} />
+          </div>
+
+          <p>Calories: {data.calories}</p>
+          <p>Servings {data.servings}</p>
+
+          <p>
+            <b>Total Calories: {data.calories * data.servings} </b> kcal
+          </p>
+
+          <button onClick={() => onDelete(data.id)}>Delete</button>
+    </div>
+  );
+}
